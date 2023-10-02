1d46a894510310fe8ffa7e2c3bb9e99259ee02ae
Felipe Cardoso <agn0stkp@gmail.com>
2023-10-02 16:22:05 +0000

    add slice comparison

--- tester/tester.go
+++ tester/tester.go
@@ -1,6 +1,9 @@
 package tester
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 type TestSuite struct {
 	Tests map[int]TestCase
@@ -16,7 +19,18 @@ func RunTestSuite(testSuite TestSuite, function TestFunction) {
 	for testIndex, testCase := range testSuite.Tests {
 		expected := testCase.Expected
 		actual := function(testCase.Args)
-		if expected != actual {
+		var success bool = true
+		if reflect.TypeOf(expected).String() == "[]int" {
+			for i := 0; i < len(expected.([]int)); i++ {
+				if expected.([]int)[i] != actual.([]int)[i] {
+					success = false
+					break
+				}
+			}
+		} else {
+			success = expected != actual
+		}
+		if !success {
 			fmt.Printf("Test %v | (E) %v | %v (A)\n", testIndex, expected, actual)
 		} else {
 			fmt.Printf("Test %v | OK\n", testIndex)
