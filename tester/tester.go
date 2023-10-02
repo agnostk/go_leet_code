8097ad879550f0cea1e4faf0ac9a036ac1f6cf7d
Felipe Cardoso <agn0stkp@gmail.com>
2023-10-02 22:08:45 +0000

    add support to [][]string type in tester

--- tester/tester.go
+++ tester/tester.go
@@ -21,19 +21,46 @@ func RunTestSuite(testSuite TestSuite, function TestFunction) {
 		actual := function(testCase.Args)
 		var success bool = true
 		if reflect.TypeOf(expected).String() == "[]int" {
-			for i := 0; i < len(expected.([]int)); i++ {
-				if expected.([]int)[i] != actual.([]int)[i] {
+			if len(expected.([]int)) != len(actual.([]int)) {
+				success = false
+			} else {
+				for i := 0; i < len(expected.([]int)); i++ {
+					if expected.([]int)[i] != actual.([]int)[i] {
+						success = false
+						break
+					}
+				}
+			}
+		} else if reflect.TypeOf(expected).String() == "[][]string" {
+			defer func() {
+				if r := recover(); r != nil {
 					success = false
-					break
+				}
+			}()
+			if len(expected.([][]string)) != len(actual.([][]string)) {
+				success = false
+			} else {
+			outerLoop:
+				for i := 0; i < len(expected.([][]string)); i++ {
+					if len(expected.([][]string)[i]) != len(actual.([][]string)[i]) {
+						success = false
+						break outerLoop
+					}
+					for j := 0; j < len(expected.([][]string)[i]); j++ {
+						if expected.([][]string)[i][j] != actual.([][]string)[i][j] {
+							success = false
+							break outerLoop
+						}
+					}
 				}
 			}
 		} else {
-			success = expected != actual
+			success = expected == actual
 		}
-		if !success {
-			fmt.Printf("Test %v | (E) %v | %v (A)\n", testIndex, expected, actual)
-		} else {
+		if success {
 			fmt.Printf("Test %v | OK\n", testIndex)
+		} else {
+			fmt.Printf("Test %v | (E) %v | %v (A)\n", testIndex, expected, actual)
 		}
 	}
 }
